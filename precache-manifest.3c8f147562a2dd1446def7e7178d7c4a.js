9e2e89d4305854ed927e8c56d9f2f59bd061ba65
pawelNobis <pawel.nobis1@gmail.com>
2019-02-25 06:46:08 +0100

    Updates

---
+++ precache-manifest.3c8f147562a2dd1446def7e7178d7c4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7edbf792605c6d07a8b5",
+    "url": "/memento/static/js/runtime~main.7edbf792.js"
+  },
+  {
+    "revision": "9608b7a5507bbc6b46eb",
+    "url": "/memento/static/js/main.9608b7a5.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/memento/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "9608b7a5507bbc6b46eb",
+    "url": "/memento/static/css/main.9c0d2b35.chunk.css"
+  },
+  {
+    "revision": "a60cf2f953cb6afdf6d270da9f74fbbb",
+    "url": "/memento/index.html"
+  }
+];
